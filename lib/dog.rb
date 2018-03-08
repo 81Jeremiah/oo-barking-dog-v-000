c6911af3f38a502ed57893d8a79e6832961a602e
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-08 17:35:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,11 @@
 # Your code goes here!
 class Dog
+  attr_accessor name:
   def initialzie (name)
     @name = name
   end
 
-  def name=(name)
-    @name = name
-  end
 
-  def name
-    @name
-  end
 
   def bark
     puts "woof!"
